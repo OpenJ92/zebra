5ac060083964f42c219c0d783f4b96633deb08b7
OpenJ92 <jacob.vartuli.92@gmail.com>
2019-12-27 17:31:36 -0500

    many pipes from NETWORK folder. Look to add metadata to pipe ymls so that they're also nodes that point to each other.

--- src/PROCESS/process.py
+++ src/PROCESS/process.py
@@ -1,18 +1,25 @@
 from yaml import load, FullLoader
 from os.path import expanduser
+from os import listdir
 
 from src.NODE.NODE import NODE
 from src.PIPE.PIPE import PIPE
 
 class process(object):
     def __init__(self, conf = None):
-        # how do we adjust s.t. we read from a directory of 
-        # pipes / load files.
-        self._config = self._process_config(conf)
-        self._pipe = PIPE(self._config)
+        self._pipes = [f"./NETWORK/{pipe}"             \
+                 for pipe in listdir('./NETWORK')      \
+                 if "PIPE" in pipe]
+        self._confs = [self._process_config(pipe) for pipe in self._pipes]
+        pipes = [PIPE(conf) for conf in self._confs]
+
+       # self._config = self._process_config(conf)
+       # self._pipe = PIPE(self._config)
 
     def _process_config(self, conf):
-        conf=f"{expanduser('~')}/.dt/config.yml" if not conf else f"{conf}" 
+        conf=f"{expanduser('~')}/.dt/config.yml"      \
+                if not conf else                      \
+                f"{conf}" 
         with open(conf, 'r') as config:
             return load(config, Loader=FullLoader)
 
